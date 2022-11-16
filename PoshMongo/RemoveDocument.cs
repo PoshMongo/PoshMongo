a00c31d6575e2d8f4033ba6aaf537361747eaa7f
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 08:24:26 -0600

    Added RemoveDocument cmdlet

---
+++ PoshMongo/RemoveDocument.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Management.Automation;
+
+namespace PoshMongo
+{
+    [Cmdlet(VerbsCommon.Remove, "Document")]
+    public class RemoveDocument : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string? DocumentId { get; set; }
+        protected override void BeginProcessing()
+        {
+            IMongoCollection<BsonDocument> Collection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
+            FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", DocumentId);
+            WriteObject(Collection.DeleteOne(filter));
+        }
+    }
+}
