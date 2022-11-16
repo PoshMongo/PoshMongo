30744d59d2fa9ebfc89cbfe6eea11146b3d7832b
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 08:09:16 -0600

    Added GetCollection cmdlet

---
+++ PoshMongo/GetCollection.cs
@@ -0,0 +1,30 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Management.Automation;
+
+namespace PoshMongo
+{
+    [Cmdlet(VerbsCommon.Get, "Collection")]
+    public class GetCollection : PSCmdlet
+    {
+        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "Default")]
+        public string? CollectionName { get; set; }
+        protected override void BeginProcessing()
+        {
+            MongoDatabaseBase Database = (MongoDatabaseBase)SessionState.PSVariable.Get("Database").Value;
+            MongoCollectionSettings settings = new();
+            if (!(string.IsNullOrEmpty(CollectionName)))
+            {
+                SessionState.PSVariable.Set("Collection", Database.GetCollection<BsonDocument>(CollectionName, settings));
+                WriteObject(SessionState.PSVariable.Get("Collection").Value);
+            }
+            else
+            {
+                foreach (string collectionName in Database.ListCollectionNames().ToEnumerable())
+                {
+                    WriteObject(Database.GetCollection<BsonDocument>(collectionName, settings));
+                }
+            }
+        }
+    }
+}
