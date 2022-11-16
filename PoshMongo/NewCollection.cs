ba67f26f7649188e5dc45bc39277e6077c2bee54
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 08:14:24 -0600

    Added NewCollection cmdlet

---
+++ PoshMongo/NewCollection.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Management.Automation;
+
+namespace PoshMongo
+{
+    [Cmdlet(VerbsCommon.New, "Collection")]
+    public class NewCollection : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string? CollectionName { get; set; }
+        protected override void BeginProcessing()
+        {
+            MongoDatabaseBase Database = (MongoDatabaseBase)SessionState.PSVariable.Get("Database").Value;
+            Database.CreateCollection(CollectionName, new CreateCollectionOptions(), new CancellationToken());
+            MongoCollectionSettings settings = new();
+            SessionState.PSVariable.Set("Collection", Database.GetCollection<BsonDocument>(CollectionName, settings));
+            WriteObject(SessionState.PSVariable.Get("Collection").Value);
+        }
+    }
+}
