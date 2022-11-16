25033f1e85f98945cb92afc38f189f0aed339bd9
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 08:06:05 -0600

    Added RemoveDatabase Cmdlet

---
+++ PoshMongo/RemoveDatabase.cs
@@ -0,0 +1,21 @@
+﻿using MongoDB.Driver;
+using System.Management.Automation;
+
+namespace PoshMongo
+{
+    [Cmdlet(VerbsCommon.Remove, "Database")]
+    public class RemoveDatabase : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string? DatabaseName { get; set; }
+        protected override void BeginProcessing()
+        {
+            MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+            Client.DropDatabase(DatabaseName);
+            foreach (string db in Client.ListDatabaseNames().ToEnumerable())
+            {
+                WriteObject(Client.GetDatabase(db));
+            }
+        }
+    }
+}
