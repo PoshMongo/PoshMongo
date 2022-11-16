bfae7fdb7d4474b18accff825d14e720b650dd2a
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 08:03:23 -0600

    Adding GetDatabase Cmdlet

---
+++ PoshMongo/GetDatabase.cs
@@ -0,0 +1,28 @@
+﻿using MongoDB.Driver;
+using System.Management.Automation;
+
+namespace PoshMongo
+{
+    [Cmdlet(VerbsCommon.Get, "Database")]
+    public class GetDatabase : PSCmdlet
+    {
+        [Parameter(Mandatory = false, ParameterSetName = "Default")]
+        public string? DatabaseName { get; set; }
+        protected override void BeginProcessing()
+        {
+            MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+            if (!(string.IsNullOrEmpty(DatabaseName)))
+            {
+                SessionState.PSVariable.Set("Database", Client.GetDatabase(DatabaseName));
+                WriteObject(SessionState.PSVariable.Get("Database").Value);
+            }
+            else
+            {
+                foreach (string db in Client.ListDatabaseNames().ToEnumerable())
+                {
+                    WriteObject(Client.GetDatabase(db));
+                }
+            }
+        }
+    }
+}
