755109fb988ee795f498faec7d53460ad500337d
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 08:07:12 -0600

    Added NewDatabase Cmdlet

---
+++ PoshMongo/NewDatabase.cs
@@ -0,0 +1,18 @@
+﻿using MongoDB.Driver;
+using System.Management.Automation;
+
+namespace PoshMongo
+{
+    [Cmdlet(VerbsCommon.New, "Database")]
+    public class NewDatabase : PSCmdlet
+    {
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
+        public string? DatabaseName { get; set; }
+        protected override void BeginProcessing()
+        {
+            MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+            SessionState.PSVariable.Set("Database", Client.GetDatabase(DatabaseName));
+            WriteObject(SessionState.PSVariable.Get("Database").Value);
+        }
+    }
+}
