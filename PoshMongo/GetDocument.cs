cac3f11044bd6bab5a6bd5cb4522de56243c3f00
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 11:25:51 -0600

    Added a filter parameter as a hashtable see #1

--- PoshMongo/GetDocument.cs
+++ PoshMongo/GetDocument.cs
@@ -1,5 +1,6 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Driver;
+using System.Collections;
 using System.Management.Automation;
 
 namespace PoshMongo
@@ -7,23 +8,39 @@ namespace PoshMongo
     [Cmdlet(VerbsCommon.Get, "Document")]
     public class GetDocument : PSCmdlet
     {
-        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "Default")]
+        [Parameter(Mandatory = false, Position = 0)]
         public string? DocumentId { get; set; }
+        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "Filter")]
+        public Hashtable? Filter { get; set; }
         protected override void BeginProcessing()
         {
             IMongoCollection<BsonDocument> Collection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
             BsonDocument bsonDocument = new();
-            if (!(string.IsNullOrEmpty(DocumentId)))
+            switch (ParameterSetName)
             {
-                FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", DocumentId);
-                WriteObject(Collection.Find(filter).FirstOrDefault().ToJson());
-            }
-            else
-            {
-                foreach (BsonDocument document in Collection.Find(bsonDocument).ToList())
-                {
-                    WriteObject(document.ToJson());
-                }
+                case "Filter":
+                    List<FilterDefinition<BsonDocument>> filters = new List<FilterDefinition<BsonDocument>>();
+                    foreach (string key in Filter.Keys)
+                    {
+                        filters.Add(Builders<BsonDocument>.Filter.Eq(key, Filter[key]));
+                    }
+                    FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.And(filters);
+                    WriteObject(Collection.Find(filter).FirstOrDefault().ToJson());
+                    break;
+                default:
+                    if (!(string.IsNullOrEmpty(DocumentId)))
+                    {
+                        FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", DocumentId);
+                        WriteObject(Collection.Find(id).FirstOrDefault().ToJson());
+                    }
+                    else
+                    {
+                        foreach (BsonDocument document in Collection.Find(bsonDocument).ToList())
+                        {
+                            WriteObject(document.ToJson());
+                        }
+                    }
+                    break;
             }
         }
     }
