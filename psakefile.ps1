c7703ab3845200cf05b5a2915650bfdb714a9aa3
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-16 23:10:33 -0600

    Updated with correct values for porject

--- psakefile.ps1
+++ psakefile.ps1
@@ -1,6 +1,6 @@
 Task default -depends UpdateReadme
 
-Task SetupModule -Description "Setup the PowerShell Module" -depends CreateModuleDirectory, CleanProject, BuildProject, CopyModuleFiles, CreateExternalHelp, CreateCabFile, CreateNuSpec, NugetPack, NugetPush
+Task SetupModule -Description "Setup the PowerShell Module" -depends CreateModuleDirectory, CleanProject, BuildProject, CopyModuleFiles, CreateExternalHelp, CreateCabFile, CreateNuSpec, NugetPack, NugetPush, RemoveModuleDirectory
 
 Task UpdateReadme -Description "Update the README file" -depends CreateModuleDirectory, CleanProject, BuildProject, CopyModuleFiles, RemoveModuleDirectory -Action {
  $moduleName =  'PoshMongo'
@@ -8,12 +8,12 @@ Task UpdateReadme -Description "Update the README file" -depends CreateModuleDir
 
  $TableHeaders = "| Latest Version | PowerShell Gallery | Build Status | Project Board | Issues | License |"
  $Columns = "|-----------------|-----------------|----------------|----------------|----------------|----------------|"
- $VersionBadge = "[![Latest Version](https://img.shields.io/github/v/tag/SchemaModule/PowerShell)](https://github.com/SchemaModule/PowerShell/tags)"
- $GalleryBadge = "[![Powershell Gallery](https://img.shields.io/powershellgallery/dt/schema)](https://www.powershellgallery.com/packages/schema)"
+ $VersionBadge = "[![Latest Version](https://img.shields.io/github/v/tag/PoshMongo/PoshMongo)](https://github.com/PoshMongo/PoshMongo/tags)"
+ $GalleryBadge = "[![Powershell Gallery](https://img.shields.io/powershellgallery/dt/PoshMongo)](https://www.powershellgallery.com/packages/PoshMongo)"
  $StatusBadge = "[![Build Status](https://dev.azure.com/patton-tech/SchemaModule/_apis/build/status/SchemaModule.PowerShell?repoName=SchemaModule%2FPowerShell&branchName=master)](https://dev.azure.com/patton-tech/SchemaModule/_build/latest?definitionId=9&repoName=SchemaModule%2FPowerShell&branchName=master)"
  $BoardBadge = "[![Board Status](https://dev.azure.com/patton-tech/cbaf64d2-5310-475c-8874-fb2b809e3f3b/1138b36a-5cb9-4d7d-8163-32041295538f/_apis/work/boardbadge/d9f5bd6d-43e7-45ac-a1aa-f1afc7f0cb17?columnOptions=1)](https://dev.azure.com/patton-tech/cbaf64d2-5310-475c-8874-fb2b809e3f3b/_boards/board/t/1138b36a-5cb9-4d7d-8163-32041295538f/Microsoft.RequirementCategory/)"
- $IssueBadge = "[![GitHub issues](https://img.shields.io/github/issues/SchemaModule/PowerShell)](https://github.com/SchemaModule/PowerShell/issues)"
- $LicenseBadge = "[![GitHub license](https://img.shields.io/github/license/SchemaModule/PowerShell)](https://github.com/SchemaModule/PowerShell/blob/master/LICENSE)"
+ $IssueBadge = "[![GitHub issues](https://img.shields.io/github/issues/PoshMongo/PoshMongo)](https://github.com/PoshMongo/PoshMongo/issues)"
+ $LicenseBadge = "[![GitHub license](https://img.shields.io/github/license/PoshMongo/PoshMongo)](https://github.com/PoshMongo/PoshMongo/blob/master/LICENSE)"
 
  if (!(Get-Module -Name $moduleName )) {Import-Module -Name ".\$($moduleName)\Module\$($moduleName).psd1" }
 
@@ -73,5 +73,6 @@ Task NugetPack -Description "Pack the nuget file" -Action {
 }
 
 Task NugetPush -Description "Push nuget to PowerShell Gallery" -Action {
- nuget push .\Module\*.nupkg
+	$config = [xml](Get-Content .\nuget.config)
+ nuget push .\Module\*.nupkg -NonInteractive -ApiKey "$($config.configuration.apikeys.add.value)" -ConfigFile .\nuget.config
 }
