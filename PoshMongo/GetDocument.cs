7c4b382206edcd3c8238b45e03d2d36026cfa2e5
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-22 11:38:44 -0600

    Added the option to supress _id from output

--- PoshMongo/GetDocument.cs
+++ PoshMongo/GetDocument.cs
@@ -18,6 +18,10 @@ namespace PoshMongo.Document
         public Hashtable? Filter { get; set; }
         [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Collection")]
         public string? CollectionName { get; set; }
+        [Parameter(Mandatory = false, ParameterSetName = "DocumentId")]
+        [Parameter(Mandatory = false, ParameterSetName = "Collection")]
+        [Parameter(Mandatory = false, ParameterSetName = "Filter")]
+        public SwitchParameter HideId { get; set; }
         protected override void ProcessRecord()
         {
             IMongoCollection<BsonDocument> Collection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
@@ -26,17 +30,17 @@ namespace PoshMongo.Document
                 case "Filter":
                     if (!(Filter == null))
                     {
-                        WriteObject(GetDocument(Collection, Filter));
+                        WriteObject(GetDocument(Collection, Filter, HideId));
                     }
                     break;
                 case "DocumentId":
                     if (!(string.IsNullOrEmpty(DocumentId)))
                     {
-                        WriteObject(GetDocument(Collection, DocumentId));
+                        WriteObject(GetDocument(Collection, DocumentId, HideId));
                     }
                     else
                     {
-                        WriteObject(GetDocument(Collection));
+                        WriteObject(GetDocument(Collection, HideId));
                     }
                     break;
                 case "Collection":
@@ -46,42 +50,75 @@ namespace PoshMongo.Document
                         ObjectId objectId;
                         if (ObjectId.TryParse(DocumentId, out objectId))
                         {
-                            WriteObject(GetDocument(Database.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings()), objectId));
+                            WriteObject(GetDocument(Database.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings()), objectId, HideId));
                         } else
                         {
-                            WriteObject(GetDocument(Database.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings()), DocumentId));
+                            WriteObject(GetDocument(Database.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings()), DocumentId, HideId));
                         }
                     }
                     else
                     {
-                        WriteObject(GetDocument(Database.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings())));
+                        WriteObject(GetDocument(Database.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings()), HideId));
                     }
                     break;
             }
         }
-        private string GetDocument(IMongoCollection<BsonDocument> Collection)
+        private string GetDocument(IMongoCollection<BsonDocument> Collection, bool noId)
         {
-            return Collection.AsQueryable().ToList().ToJson();
+            //return Collection.AsQueryable().ToList().ToJson();
+            if (noId == true)
+            {
+                ProjectionDefinition<BsonDocument> projection = Builders<BsonDocument>.Projection.Exclude("_id");
+                return Collection.Find(new BsonDocument()).Project(projection).ToList().ToJson();
+            }
+            else
+            {
+                return Collection.Find(new BsonDocument()).ToList().ToJson();
+            }
         }
-        private string GetDocument(IMongoCollection<BsonDocument> Collection, string Id)
+        private string GetDocument(IMongoCollection<BsonDocument> Collection, string Id, bool noId)
         {
             FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", Id);
-            return Collection.Find(id).FirstOrDefault().ToJson();
+            if (noId == true)
+            {
+                ProjectionDefinition<BsonDocument> projection = Builders<BsonDocument>.Projection.Exclude("_id");
+                return Collection.Find(id).Project(projection).FirstOrDefault().ToJson();
+            }
+            else
+            {
+                return Collection.Find(id).FirstOrDefault().ToJson();
+            }
         }
-        private string GetDocument(IMongoCollection<BsonDocument> Collection, ObjectId Id)
+        private string GetDocument(IMongoCollection<BsonDocument> Collection, ObjectId Id, bool noId)
         {
-            FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(Id.ToString()));            
-            return Collection.Find(id).FirstOrDefault().ToJson();
+            FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(Id.ToString()));
+            if (noId == true)
+            {
+                ProjectionDefinition<BsonDocument> projection = Builders<BsonDocument>.Projection.Exclude("_id");
+                return Collection.Find(id).Project(projection).FirstOrDefault().ToJson();
+            }
+            else
+            {
+                return Collection.Find(id).FirstOrDefault().ToJson();
+            }
         }
-        private string GetDocument(IMongoCollection<BsonDocument> Collection,Hashtable filter)
+        private string GetDocument(IMongoCollection<BsonDocument> Collection,Hashtable filter, bool noId)
         {
             List<FilterDefinition<BsonDocument>> filters = new List<FilterDefinition<BsonDocument>>();
             foreach (string key in filter.Keys)
             {
                 filters.Add(Builders<BsonDocument>.Filter.Eq(key, filter[key]));
             }
-            FilterDefinition<BsonDocument> result = Builders<BsonDocument>.Filter.And(filters);
-            return Collection.Find(result).FirstOrDefault().ToJson();
+            FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.And(filters);
+            if (noId == true)
+            {
+                ProjectionDefinition<BsonDocument> projection = Builders<BsonDocument>.Projection.Exclude("_id");
+                return Collection.Find(id).Project(projection).FirstOrDefault().ToJson();
+            }
+            else
+            {
+                return Collection.Find(id).FirstOrDefault().ToJson();
+            }
         }
         private void SetVariable(string VariableName, object Value)
         {
