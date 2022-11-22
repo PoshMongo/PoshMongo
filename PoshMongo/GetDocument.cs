f7098fd5033f08280a8225c5b8c5318c44e2f092
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-22 00:15:46 -0600

    Changes to line up current code to make adding new functionality easier

--- PoshMongo/GetDocument.cs
+++ PoshMongo/GetDocument.cs
@@ -2,51 +2,65 @@
 using MongoDB.Driver;
 using System.Collections;
 using System.Management.Automation;
+using System.Text.Json;
+using System.Text.Json.Nodes;
 
 namespace PoshMongo.Document
 {
     [Cmdlet(VerbsCommon.Get, "Document")]
     [OutputType("System.Text.Json")]
     [CmdletBinding(HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Get-MongoDBDocument.md#get-mongodbdocument", PositionalBinding = true)]
-    public class GetDocument : PSCmdlet
+    public class GetDocumentCmdlet : PSCmdlet
     {
         [Parameter(Mandatory = false, Position = 0)]
         public string? DocumentId { get; set; }
         [Parameter(Mandatory = false, Position = 0, ParameterSetName = "Filter")]
         public Hashtable? Filter { get; set; }
-        protected override void BeginProcessing()
+        protected override void ProcessRecord()
         {
             IMongoCollection<BsonDocument> Collection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
-            BsonDocument bsonDocument = new();
             switch (ParameterSetName)
             {
                 case "Filter":
                     if (!(Filter == null))
                     {
-                        List<FilterDefinition<BsonDocument>> filters = new List<FilterDefinition<BsonDocument>>();
-                        foreach (string key in Filter.Keys)
-                        {
-                            filters.Add(Builders<BsonDocument>.Filter.Eq(key, Filter[key]));
-                        }
-                        FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.And(filters);
-                        WriteObject(Collection.Find(filter).FirstOrDefault().ToJson());
+                        WriteObject(GetDocument(Collection, Filter));
                     }
                     break;
                 default:
                     if (!(string.IsNullOrEmpty(DocumentId)))
                     {
-                        FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", DocumentId);
-                        WriteObject(Collection.Find(id).FirstOrDefault().ToJson());
+                        WriteObject(GetDocument(Collection, DocumentId));
                     }
                     else
                     {
-                        foreach (BsonDocument document in Collection.Find(bsonDocument).ToList())
-                        {
-                            WriteObject(document.ToJson());
-                        }
+                        WriteObject(GetDocument(Collection));
                     }
                     break;
             }
+        }
+        private string GetDocument(IMongoCollection<BsonDocument> Collection)
+        {
+            return Collection.AsQueryable().ToList().ToJson();
+        }
+        private string GetDocument(IMongoCollection<BsonDocument> Collection, string Id)
+        {
+            FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", Id);
+            return Collection.Find(id).FirstOrDefault().ToJson();
+        }
+        private string GetDocument(IMongoCollection<BsonDocument> Collection,Hashtable filter)
+        {
+            List<FilterDefinition<BsonDocument>> filters = new List<FilterDefinition<BsonDocument>>();
+            foreach (string key in filter.Keys)
+            {
+                filters.Add(Builders<BsonDocument>.Filter.Eq(key, filter[key]));
+            }
+            FilterDefinition<BsonDocument> result = Builders<BsonDocument>.Filter.And(filters);
+            return Collection.Find(result).FirstOrDefault().ToJson();
+        }
+        private void SetVariable(string VariableName, object Value)
+        {
+            SessionState.PSVariable.Set(VariableName, Value);
         }
     }
 }
