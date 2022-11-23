a98193f73b159a5c9182a928415d0c516cc28448
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-23 12:48:14 -0600

    Rewriting tests for parametersets

--- Tests/Get-MongoDBCollection.Tests.ps1
+++ Tests/Get-MongoDBCollection.Tests.ps1
@@ -22,34 +22,77 @@ Describe "Get-MongoDBCollection" -Tag $Module, "GetCollectionCmdlet", "Collectio
   }
  }
  Context "Get-MongoDBCollection Usage" {
-  Context "Without a CollectionName" {
-   It "Should Return MongoDB.Driver.MongoCollectionImpl`1[MongoDB.Bson.BsonDocument]" {
-    (Get-MongoDBCollection).GetType().FullName | Should -Be 'System.Collections.Generic.List`1[[MongoDB.Driver.IMongoCollection`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]], MongoDB.Driver, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+  Context "Get-MongoDBCollection CollectionName ParameterSet" {
+   Context "Without a CollectionName" {
+    It "Should Return MongoDB.Driver.MongoCollectionImpl`1[MongoDB.Bson.BsonDocument]" {
+     (Get-MongoDBCollection).GetType().FullName | Should -Be 'System.Collections.Generic.List`1[[MongoDB.Driver.IMongoCollection`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]], MongoDB.Driver, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
+   }
+   Context "With a CollectionName" {
+    It "Should Return MongoDB.Driver.IMongoCollection" {
+     (Get-MongoDBCollection -CollectionName 'myCollection').GetType().FullName | Should -Be 'MongoDB.Driver.MongoCollectionImpl`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
+   }
+   Context "With an invalid CollectionName" {
+    It "Should throw an error: Database names must be non-empty and not contain '.' or the null character." {
+     { Get-MongoDBCollection -CollectionName " " } | Should -Throw
+    }
    }
   }
-  Context "With a CollectionName" {
-   It "Should Return MongoDB.Driver.IMongoCollection" {
-    (Get-MongoDBCollection -CollectionName 'myCollection').GetType().FullName | Should -Be 'MongoDB.Driver.MongoCollectionImpl`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+  Context "Get-MongoDBCollection CollectionNameSpace ParameterSet" {
+   Context "With a CollectionNamespace" {
+    It "Should Return MongoDB.Driver.IMongoCollection" {
+     (Get-MongoDBCollection -CollectionNamespace 'MyDB.myCollection').GetType().FullName | Should -Be 'MongoDB.Driver.MongoCollectionImpl`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
+   }
+   Context "Without a CollectionNamespace" {
+    It "Should throw an error: Missing an argument for parameter 'CollectionNamespace'." {
+     { Get-MongoDBCollection -CollectionNamespace '' } | Should -Throw "Cannot bind argument to parameter 'CollectionNamespace' because it is an empty string."
+    }
    }
   }
-  Context "With a CollectionNamespace" {
-   It "Should Return MongoDB.Driver.IMongoCollection" {
-    (Get-MongoDBCollection -CollectionNamespace 'MyDB.myCollection').GetType().FullName | Should -Be 'MongoDB.Driver.MongoCollectionImpl`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+  Context "Get-MongoDBCollection DatabaseName ParameterSet" {
+   Context "With a DatabaseName" {
+    It "Should Return MongoDB.Driver.IMongoCollection" {
+     (Get-MongoDBCollection -DatabaseName 'MyDB').GetType().FullName | Should -Be 'System.Collections.Generic.List`1[[MongoDB.Driver.IMongoCollection`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]], MongoDB.Driver, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
+   }
+   Context "With a DatabaseName and CollectionName" {
+    It "Should Return MongoDB.Driver.IMongoCollection" {
+     (Get-MongoDBCollection -DatabaseName 'MyDB' -CollectionName 'myCollection2').GetType().FullName | Should -Be 'MongoDB.Driver.MongoCollectionImpl`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
+   }
+   Context "Without a DatabaseName" {
+    It "Should throw an error: Cannot bind argument to parameter 'DatabaseName'" {
+     { Get-MongoDBCollection -DatabaseName '' -CollectionName 'myCollection2' } | Should -Throw "Cannot bind argument to parameter 'DatabaseName' because it is an empty string."
+    }
+   }
+   Context "Without a CollectionName" {
+    It "Should Return MongoDB.Driver.MongoCollectionImpl`1[MongoDB.Bson.BsonDocument]" {
+     (Get-MongoDBCollection -DatabaseName 'MyDB' -CollectionName '').GetType().FullName | Should -Be 'System.Collections.Generic.List`1[[MongoDB.Driver.IMongoCollection`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]], MongoDB.Driver, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
    }
   }
-  Context "Without a CollectionNamespace" {
-   It "Should throw an error: Missing an argument for parameter 'CollectionNamespace'." {
-    { Get-MongoDBCollection -CollectionNamespace '' } | Should -Throw "Cannot bind argument to parameter 'CollectionNamespace' because it is an empty string."
+  Context "Get-MongoDBCollection Database ParameterSet" {
+   Context "With a Database" {
+    It "Should Return MongoDB.Driver.IMongoCollection" {
+     (Get-MongoDBCollection -MongoDatabase $Database).GetType().FullName | Should -Be 'System.Collections.Generic.List`1[[MongoDB.Driver.IMongoCollection`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]], MongoDB.Driver, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
    }
-  }
-  Context "With a DatabaseName" {
-   It "Should Return MongoDB.Driver.IMongoCollection" {
-    (Get-MongoDBCollection -DatabaseName 'MyDB').GetType().FullName | Should -Be 'System.Collections.Generic.List`1[[MongoDB.Driver.IMongoCollection`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]], MongoDB.Driver, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+   Context "With a Database and CollectionName" {
+    It "Should Return MongoDB.Driver.IMongoCollection" {
+     (Get-MongoDBCollection -MongoDatabase $Database -CollectionName 'myCollection2').GetType().FullName | Should -Be 'MongoDB.Driver.MongoCollectionImpl`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
    }
-  }
-  Context "With an invalid CollectionName" {
-   It "Should throw an error: Database names must be non-empty and not contain '.' or the null character." {
-    { Get-MongoDBCollection -CollectionName " " } | Should -Throw
+   Context "Without a Database" {
+    It "Should throw and error: Cannot bind argument to parameter 'MongoDatabase'" {
+     { Get-MongoDBCollection -MongoDatabase $null } | Should -Throw "Cannot bind argument to parameter 'MongoDatabase' because it is null."
+    }
+   }
+   Context "Without a CollectionName" {
+    It "Should Return MongoDB.Driver.MongoCollectionImpl`1[MongoDB.Bson.BsonDocument]" {
+     (Get-MongoDBCollection -MongoDatabase $Database -CollectionName '').GetType().FullName | Should -Be 'System.Collections.Generic.List`1[[MongoDB.Driver.IMongoCollection`1[[MongoDB.Bson.BsonDocument, MongoDB.Bson, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]], MongoDB.Driver, Version=2.18.0.0, Culture=neutral, PublicKeyToken=null]]'
+    }
    }
   }
  }
