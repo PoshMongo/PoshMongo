8fc50710a5a8eec2362e36d61992675ba3c43c1f
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-28 16:51:03 -0600

    AddDocumentCmdlet now supports the pipeline

--- PoshMongo/AddDocument.cs
+++ PoshMongo/AddDocument.cs
@@ -5,22 +5,46 @@ using System.Reflection.Metadata;
 
 namespace PoshMongo.Document
 {
-    [Cmdlet(VerbsCommon.Add, "Document")]
+    [Cmdlet(VerbsCommon.Add, "Document", HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Add-MongoDBDocument.md#add-mongodbdocument")]
     [OutputType("System.Text.Json")]
-    [CmdletBinding(HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Add-MongoDBDocument.md#add-mongodbdocument", PositionalBinding = true)]
+    [CmdletBinding(PositionalBinding = true, DefaultParameterSetName = "Default")]
     public class AddDocumentCmdlet : PSCmdlet
     {
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
-        public string? Document { get; set; }
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionName")]
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Collection")]
+        public string Document { get; set; } = string.Empty;
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionName")]
+        public string CollectionName { get; set; } = string.Empty;
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Collection", ValueFromPipeline = true)]
+        public IMongoCollection<BsonDocument>? MongoCollection { get; set; }
         protected override void ProcessRecord()
         {
-            IMongoCollection<BsonDocument> Collection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
-            if (!(string.IsNullOrEmpty(Document)))
+            if (MongoCollection == null)
             {
-                WriteObject(AddDocument(Collection, Document));
+                if (!(string.IsNullOrEmpty(CollectionName)))
+                {
+                    MongoDatabaseBase MongoDatabase = (MongoDatabaseBase)SessionState.PSVariable.Get("Database").Value;
+                    MongoCollection = MongoDatabase.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings());
+                }
+                else
+                {
+                    MongoCollection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
+                }
+            }
+            WriteVerbose(ParameterSetName);
+            switch (ParameterSetName)
+            {
+                case "CollectionName":
+                    WriteObject(AddDocument(MongoCollection, Document));
+                    break;
+                default:
+                    MongoCollection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
+                    WriteObject(AddDocument(MongoCollection, Document));
+                    break;
             }
         }
-        private string AddDocument(IMongoCollection<BsonDocument> Collection, string document)
+        private static string AddDocument(IMongoCollection<BsonDocument> Collection, string document)
         {
             BsonDocument bsonDocument = BsonDocument.Parse(document);
             Collection.InsertOne(bsonDocument);
