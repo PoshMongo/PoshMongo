334adb8e42f3945e3c2732c7133631a8d81eceae
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-30 14:48:19 -0600

    Updated RemoveCmdlet with Client param

--- PoshMongo/RemoveDatabase.cs
+++ PoshMongo/RemoveDatabase.cs
@@ -1,18 +1,32 @@
 ﻿using MongoDB.Driver;
+using MongoDB.Driver.Core.Servers;
 using System.Management.Automation;
 
 namespace PoshMongo.Database
 {
-    [Cmdlet(VerbsCommon.Remove, "Database")]
+    [Cmdlet(VerbsCommon.Remove, "Database", HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Remove-MongoDBDatabase0.md#remove-mongodbdatabase")]
     [OutputType("MongoDB.Driver.MongoDatabaseBase")]
-    [CmdletBinding(HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Remove-MongoDBDatabase0.md#remove-mongodbdatabase", PositionalBinding = true)]
+    [CmdletBinding(PositionalBinding = true)]
     public class RemoveDatabase : PSCmdlet
     {
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
-        public string? DatabaseName { get; set; }
-        protected override void BeginProcessing()
+        public string DatabaseName { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
+        public MongoClient? Client { get; set; }
+        protected override void ProcessRecord()
         {
-            MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+            if (Client == null)
+            {
+                Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+            }
+            else
+            {
+                ServerDescription? server = Client.Cluster.Description.Servers.FirstOrDefault();
+                if (server != null)
+                {
+                    throw new MongoConnectionException(new MongoDB.Driver.Core.Connections.ConnectionId(server.ServerId), "Must be connected to a MongoDB instance.");
+                }
+            }
             Client.DropDatabase(DatabaseName);
             foreach (string db in Client.ListDatabaseNames().ToEnumerable())
             {
