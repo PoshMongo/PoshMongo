315e9f10d1036df189157ed4f1ec6a5cf7ed2dee
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-30 14:34:37 -0600

    Updated GetDatabase wtih Client parameter

--- PoshMongo/GetDatabase.cs
+++ PoshMongo/GetDatabase.cs
@@ -1,18 +1,32 @@
 ﻿using MongoDB.Driver;
+using MongoDB.Driver.Core.Servers;
 using System.Management.Automation;
 
 namespace PoshMongo.Database
 {
-    [Cmdlet(VerbsCommon.Get, "Database")]
+    [Cmdlet(VerbsCommon.Get, "Database", HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Get-MongoDBDatabase0.md#get-mongodbdatabase")]
     [OutputType("MongoDB.Driver.MongoDatabaseBase")]
-    [CmdletBinding(HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Get-MongoDBDatabase0.md#get-mongodbdatabase", PositionalBinding = true)]
+    [CmdletBinding(PositionalBinding = true)]
     public class GetDatabase : PSCmdlet
     {
         [Parameter(Mandatory = false, Position = 0, ParameterSetName = "Default")]
-        public string? DatabaseName { get; set; }
+        public string DatabaseName { get; set; } = string.Empty;
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
+        public MongoClient? Client { get; set; }
         protected override void BeginProcessing()
         {
-            MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+            if (Client == null)
+            {
+                Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+            }
+            else
+            {
+                ServerDescription? server = Client.Cluster.Description.Servers.FirstOrDefault();
+                if (server != null)
+                {
+                    throw new MongoConnectionException(new MongoDB.Driver.Core.Connections.ConnectionId(server.ServerId), "Must be connected to a MongoDB instance.");
+                }
+            }
             if (!(string.IsNullOrEmpty(DatabaseName)))
             {
                 SessionState.PSVariable.Set("Database", Client.GetDatabase(DatabaseName));
