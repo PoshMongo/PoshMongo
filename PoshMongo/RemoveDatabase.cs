20ba54b993d578dcab2bd693012236e4b366db73
Jeffrey Patton <jeffrey@patton-tech.com>
2022-11-30 16:25:26 -0600

    Added Database param as a pipeline

--- PoshMongo/RemoveDatabase.cs
+++ PoshMongo/RemoveDatabase.cs
@@ -12,7 +12,10 @@ namespace PoshMongo.Database
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
         public string DatabaseName { get; set; } = string.Empty;
         [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Database")]
         public MongoClient? Client { get; set; }
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Database", ValueFromPipeline = true)]
+        public MongoDatabaseBase? Database { get; set; }
         protected override void ProcessRecord()
         {
             if (Client == null)
@@ -27,7 +30,13 @@ namespace PoshMongo.Database
                     throw new MongoConnectionException(new MongoDB.Driver.Core.Connections.ConnectionId(server.ServerId), "Must be connected to a MongoDB instance.");
                 }
             }
-            Client.DropDatabase(DatabaseName);
+            if (Database == null)
+            {
+                Client.DropDatabase(DatabaseName);
+            } else
+            {
+                Client.DropDatabase(Database.DatabaseNamespace.DatabaseName);
+            }
             foreach (string db in Client.ListDatabaseNames().ToEnumerable())
             {
                 WriteObject(Client.GetDatabase(db));
