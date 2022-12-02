6aa431ef6151787da5af263445e8af457b41b33f
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-02 12:01:41 -0600

    Moved the HelpUri to the correct place, and updated the nullable string.

--- PoshMongo/ConnectInstance.cs
+++ PoshMongo/ConnectInstance.cs
@@ -4,13 +4,13 @@ using System.Security.Authentication;
 
 namespace PoshMongo.Connection
 {
-    [Cmdlet(VerbsCommunications.Connect, "Instance")]
+    [Cmdlet(VerbsCommunications.Connect, "Instance", HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Connect-MongoDBInstance.md#connect-mongodbinstance")]
     [OutputType("MongoDB.Driver.MongoClient")]
-    [CmdletBinding(HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Connect-MongoDBInstance.md#connect-mongodbinstance", PositionalBinding = true)]
+    [CmdletBinding(PositionalBinding = true)]
     public class ConnectInstance : PSCmdlet
     {
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
-        public string? ConnectionString { get; set; }
+        public string ConnectionString { get; set; } = string.Empty;
         [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")]
         public SwitchParameter ForceTls12 { get; set; }
         protected override void BeginProcessing()
