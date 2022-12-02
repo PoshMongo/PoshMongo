98fd09b09fc00bdc09b8d3e017e740b45d208271
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-02 10:28:05 -0600

    Reorganized and documented methods

--- PoshMongo/Operations.cs
+++ PoshMongo/Operations.cs
@@ -11,6 +11,16 @@ namespace PoshMongo
 {
     internal class Operations
     {
+        ///
+        /// Create
+        /// 
+        
+        /// <summary>
+        /// This method adds a Document to a MongoCollection
+        /// </summary>
+        /// <param name="Collection">The collection to add the document to</param>
+        /// <param name="document">A json string document</param>
+        /// <returns></returns>
         public static string AddDocument(IMongoCollection<BsonDocument> Collection, string document)
         {
             BsonDocument bsonDocument = BsonDocument.Parse(document);
@@ -18,6 +28,37 @@ namespace PoshMongo
             FilterDefinition<BsonDocument> filter = Builders<BsonDocument>.Filter.Eq("_id", bsonDocument["_id"]);
             return Collection.Find(filter).FirstOrDefault().ToJson();
         }
+        /// <summary>
+        /// Create a Collection in a MongoDatabase
+        /// </summary>
+        /// <param name="collectionName">The name of the Collection to create</param>
+        /// <param name="mongoDatabase">The MongoDatabase to create the Collection in</param>
+        /// <returns></returns>
+        public static IMongoCollection<BsonDocument> NewCollection(string collectionName, IMongoDatabase mongoDatabase)
+        {
+            mongoDatabase.CreateCollection(collectionName, new CreateCollectionOptions(), new CancellationToken());
+            IMongoCollection<BsonDocument> Collection = mongoDatabase.GetCollection<BsonDocument>(collectionName, new MongoCollectionSettings());
+            return Collection;
+        }
+        /// <summary>
+        /// Create a MongoDatabse
+        /// </summary>
+        /// <param name="Client">A Connected MongoClient</param>
+        /// <param name="DatabaseName">The name of the Database to create</param>
+        /// <returns></returns>
+        public static IMongoDatabase NewDatabase(MongoClient Client, string DatabaseName)
+        {
+            return Client.GetDatabase(DatabaseName);
+        }
+        ///
+        ///  Read
+        /// 
+
+        /// <summary>
+        /// Return a list of Collection from a MongoDatabase
+        /// </summary>
+        /// <param name="mongoDatabase"> The MongoDatabase to get a list of Collections from</param>
+        /// <returns></returns>
         public static List<IMongoCollection<BsonDocument>> GetCollection(MongoDatabaseBase mongoDatabase)
         {
             List<IMongoCollection<BsonDocument>> Collections = new();
@@ -27,15 +68,32 @@ namespace PoshMongo
             }
             return Collections;
         }
+        /// <summary>
+        /// Return a Collection from a MongoDatabase
+        /// </summary>
+        /// <param name="mongoDatabase">The MongoDatabase to a get Collection from</param>
+        /// <param name="collectionName">The Collection to get</param>
+        /// <returns></returns>
         public static IMongoCollection<BsonDocument> GetCollection(MongoDatabaseBase mongoDatabase, string collectionName)
         {
             IMongoCollection<BsonDocument> Collection = mongoDatabase.GetCollection<BsonDocument>(collectionName, new MongoCollectionSettings());
             return Collection;
         }
+        /// <summary>
+        /// Return a MongoDatabase from a MongoClient
+        /// </summary>
+        /// <param name="Client">A Connected MongoClient</param>
+        /// <param name="DatabaseName">The name of the Database to return</param>
+        /// <returns></returns>
         public static IMongoDatabase GetDatabase(MongoClient Client, string DatabaseName)
         {
             return Client.GetDatabase(DatabaseName);
         }
+        /// <summary>
+        /// Return a list of MongoDatabases from a MongoClient
+        /// </summary>
+        /// <param name="Client">A Connected MongoClient</param>
+        /// <returns></returns>
         public static List<IMongoDatabase> GetDatabase(MongoClient Client)
         {
             List<IMongoDatabase> mongoDatabases = new();
@@ -45,6 +103,12 @@ namespace PoshMongo
             }
             return mongoDatabases;
         }
+        /// <summary>
+        /// Return a list of Document from a Collection
+        /// </summary>
+        /// <param name="Collection">The Collection to get Documents from</param>
+        /// <param name="noId">Boolean to display ID field</param>
+        /// <returns></returns>
         public static List<string> GetDocument(IMongoCollection<BsonDocument> Collection, bool noId)
         {
             List<string> Documents = new();
@@ -65,6 +129,13 @@ namespace PoshMongo
             }
             return Documents;
         }
+        /// <summary>
+        /// Return a Document from a Collection by Id
+        /// </summary>
+        /// <param name="Collection">The Collection to get the Document from</param>
+        /// <param name="Id">The ID of the Document to get</param>
+        /// <param name="noId">Boolean to display ID field</param>
+        /// <returns></returns>
         public static string GetDocument(IMongoCollection<BsonDocument> Collection, string Id, bool noId)
         {
             FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", Id);
@@ -78,6 +149,13 @@ namespace PoshMongo
                 return Collection.Find(id).FirstOrDefault().ToJson();
             }
         }
+        /// <summary>
+        /// Return a Document from a Collection by Object ID
+        /// </summary>
+        /// <param name="Collection">The Collection to get the Document from</param>
+        /// <param name="Id">The ObjectID of the Document to get</param>
+        /// <param name="noId">Boolean to display ID field</param>
+        /// <returns></returns>
         public static string GetDocument(IMongoCollection<BsonDocument> Collection, ObjectId Id, bool noId)
         {
             FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", ObjectId.Parse(Id.ToString()));
@@ -91,6 +169,13 @@ namespace PoshMongo
                 return Collection.Find(id).FirstOrDefault().ToJson();
             }
         }
+        /// <summary>
+        /// R\eEturn a Document from a Collection using a Filter
+        /// </summary>
+        /// <param name="Collection">The Collection to get the Document from</param>
+        /// <param name="filter">A filter for Documents</param>
+        /// <param name="noId">Boolean to display ID field</param>
+        /// <returns></returns>
         public static string GetDocument(IMongoCollection<BsonDocument> Collection, Hashtable filter, bool noId)
         {
             List<FilterDefinition<BsonDocument>> filters = new List<FilterDefinition<BsonDocument>>();
@@ -109,29 +194,43 @@ namespace PoshMongo
                 return Collection.Find(id).FirstOrDefault().ToJson();
             }
         }
-        public static IMongoCollection<BsonDocument> NewCollection(string collectionName, IMongoDatabase mongoDatabase)
-        {
-            mongoDatabase.CreateCollection(collectionName, new CreateCollectionOptions(), new CancellationToken());
-            IMongoCollection<BsonDocument> Collection = mongoDatabase.GetCollection<BsonDocument>(collectionName, new MongoCollectionSettings());
-            return Collection;
-        }
-        public static IMongoDatabase NewDatabase(MongoClient Client, string DatabaseName)
-        {
-            return Client.GetDatabase(DatabaseName);
-        }
+        ///
+        /// Delete
+        /// 
+        
+        /// <summary>
+        /// Remove a Collection from a MongoDatabase
+        /// </summary>
+        /// <param name="mongoDatabase">The MongoDatabase to remove the Collection from</param>
+        /// <param name="collectionName">The name of the Collection to remove</param>
         public static void RemoveCollection(IMongoDatabase mongoDatabase, string collectionName)
         {
             mongoDatabase.DropCollection(collectionName);
         }
+        /// <summary>
+        /// Remove a MongoDatase
+        /// </summary>
+        /// <param name="Client">A Connected MongoClient</param>
+        /// <param name="DatabaseName">The name of the MongoDatabase to remove</param>
         public static void RemoveDatabase(MongoClient Client, string DatabaseName)
         {
             Client.DropDatabase(DatabaseName);
         }
+        /// <summary>
+        /// Remove a Document from a Collection
+        /// </summary>
+        /// <param name="Collection">The Collection to remove a Document from</param>
+        /// <param name="documentID">The ID of the Document to remove</param>
         public static void RemoveDocument(IMongoCollection<BsonDocument> Collection, string documentID)
         {
             FilterDefinition<BsonDocument> id = Builders<BsonDocument>.Filter.Eq("_id", documentID);
             Collection.DeleteOne(id);
         }
+        /// <summary>
+        /// REmove a Document from a Collection
+        /// </summary>
+        /// <param name="Collection">The Collection to remove a Document from</param>
+        /// <param name="filter">A filtered Document to remove</param>
         public static void RemoveDocument(IMongoCollection<BsonDocument> Collection, Hashtable filter)
         {
             List<FilterDefinition<BsonDocument>> filters = new List<FilterDefinition<BsonDocument>>();
