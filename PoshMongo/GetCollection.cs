8c6f7cfc214503ab367344dc94b3d8de6b354d19
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-07 15:28:28 -0600

    Updated code to not use a Database variable stored in PowerShell Global scope

--- PoshMongo/GetCollection.cs
+++ PoshMongo/GetCollection.cs
@@ -9,7 +9,6 @@ namespace PoshMongo.Collection
     [CmdletBinding(PositionalBinding = true)]
     public class GetCollectionCmdlet : PSCmdlet
     {
-        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "Collection")]
         [Parameter(Mandatory = false, Position = 1, ParameterSetName = "DatabaseName")]
         [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Database")]
         public string CollectionName { get; set; } = string.Empty;
@@ -21,12 +20,10 @@ namespace PoshMongo.Collection
         public string CollectionNamespace { get; set; } = string.Empty;
         protected override void ProcessRecord()
         {
-            if (MongoDatabase == null)
-            {
-                MongoDatabase = (MongoDatabaseBase)SessionState.PSVariable.Get("Database").Value;
-            }
+            WriteVerbose("ParameterSet: " + ParameterSetName);
             if (!(string.IsNullOrEmpty(DatabaseName)))
             {
+                WriteVerbose("DatabaseName: " + DatabaseName);
                 MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
                 MongoDatabase = (MongoDatabaseBase)Client.GetDatabase(DatabaseName);
             }
@@ -40,9 +37,12 @@ namespace PoshMongo.Collection
             switch (ParameterSetName)
             {
                 case "CollectionNamespace":
-                    if (!(string.IsNullOrEmpty(CollectionNamespace)))
+                    if (!(MongoDatabase == null))
                     {
-                        WriteObject(Operations.GetCollection(MongoDatabase, CollectionName));
+                        if (!(string.IsNullOrEmpty(CollectionNamespace)))
+                        {
+                            WriteObject(Operations.GetCollection(MongoDatabase, CollectionName));
+                        }
                     }
                     break;
                 case "Database":
@@ -59,32 +59,38 @@ namespace PoshMongo.Collection
                     }
                     break;
                 case "DatabaseName":
-                    if (!(string.IsNullOrEmpty(DatabaseName)))
+                    if (!(MongoDatabase == null))
+                    {
+                        if (!(string.IsNullOrEmpty(DatabaseName)))
+                        {
+                            WriteVerbose("DatabaseName: " + DatabaseName);
+                            if (string.IsNullOrEmpty(CollectionName))
+                            {
+                                WriteVerbose("CollectionName: Null");
+                                WriteObject(Operations.GetCollection(MongoDatabase));
+                            }
+                            else
+                            {
+                                WriteVerbose("CollectionName: " + CollectionName);
+                                WriteObject(Operations.GetCollection(MongoDatabase, CollectionName));
+                            }
+                        }
+                    }
+                    break;
+                case "Collection":
+                    if (!(MongoDatabase == null))
                     {
                         if (string.IsNullOrEmpty(CollectionName))
                         {
                             WriteObject(Operations.GetCollection(MongoDatabase));
-                        } else
+                        }
+                        else
                         {
                             WriteObject(Operations.GetCollection(MongoDatabase, CollectionName));
                         }
                     }
                     break;
-                case "Collection":
-                    if (string.IsNullOrEmpty(CollectionName))
-                    {
-                        WriteObject(Operations.GetCollection(MongoDatabase));
-                    }
-                    else
-                    {
-                        WriteObject(Operations.GetCollection(MongoDatabase, CollectionName));
-                    }
-                    break;
             }
-        }
-        private void SetVariable(string VariableName, object Value)
-        {
-            SessionState.PSVariable.Set(VariableName, Value);
         }
     }
 }
