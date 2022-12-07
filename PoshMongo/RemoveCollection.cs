dcc961d5242262457f97a0dae4fb361699268f92
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-07 16:13:11 -0600

    Updated RemoveCollection so it works with just a collection, or requires databasename and collectionanem to remove

--- PoshMongo/RemoveCollection.cs
+++ PoshMongo/RemoveCollection.cs
@@ -7,61 +7,47 @@ namespace PoshMongo.Collection
 {
     [Cmdlet(VerbsCommon.Remove, "Collection", HelpUri = "https://github.com/PoshMongo/PoshMongo/blob/master/Docs/Remove-MongoDBCollection.md#remove-mongodbcollection")]
     [OutputType("null")]
-    [CmdletBinding(PositionalBinding = true)]
+    [CmdletBinding(PositionalBinding = true, DefaultParameterSetName = "Collection")]
     public class RemoveCollectionCmdlet : PSCmdlet
     {
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionName")]
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "DatabaseName")]
         public string CollectionName { get; set; } = string.Empty;
         [Parameter(Mandatory = true, Position = 1, ParameterSetName = "DatabaseName")]
         public string DatabaseName { get; set; } = string.Empty;
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Collection", ValueFromPipeline = true)]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Collection", ValueFromPipeline = true)]
         public IMongoCollection<BsonDocument>? Collection { get; set; }
         private IMongoDatabase? MongoDatabase { get; set; }
         protected override void ProcessRecord()
         {
-            if (string.IsNullOrEmpty(DatabaseName))
-            {
-                MongoDatabase = (MongoDatabaseBase)SessionState.PSVariable.Get("Database").Value;
-            } else
-            {
-                MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
-                if (Client != null)
-                {
-                    MongoDatabase = Client.GetDatabase(DatabaseName);
-                }
-            }
-            if (Collection != null)
-            {
-                MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
-                MongoDatabase = Client.GetDatabase(Collection.CollectionNamespace.DatabaseNamespace.DatabaseName);
-            }
+            WriteVerbose("ParameterSetName : " + ParameterSetName);
             switch (ParameterSetName)
             {
-                case "CollectionName":
-                    if (!(string.IsNullOrEmpty(CollectionName)))
-                    {
-                        if (MongoDatabase != null)
-                        {
-                            Operations.RemoveCollection(MongoDatabase, CollectionName);
-                        }
-                    }
-                    break;
                 case "DatabaseName":
                     if (!(string.IsNullOrEmpty(CollectionName)) && !(string.IsNullOrEmpty(DatabaseName)))
                     {
+                        MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+                        if (Client != null)
+                        {
+                            WriteVerbose("DatabaseName: " + DatabaseName);
+                            MongoDatabase = Client.GetDatabase(DatabaseName);
+                        }
                         if (MongoDatabase != null)
                         {
+                            WriteVerbose("CollectionName: " + CollectionName);
                             Operations.RemoveCollection(MongoDatabase, CollectionName);
                         }
                     }
                     break;
-                case "Collection":
+                default:
                     if (Collection != null)
                     {
+                        WriteVerbose("DatabaseName: " + Collection.CollectionNamespace.DatabaseNamespace.DatabaseName);
+                        WriteVerbose("CollectionName: " + Collection.CollectionNamespace.CollectionName);
+                        MongoClient Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
+                        MongoDatabase = Client.GetDatabase(Collection.CollectionNamespace.DatabaseNamespace.DatabaseName);
                         if (MongoDatabase != null)
                         {
-                            Operations.RemoveCollection(MongoDatabase, CollectionName);
+                            Operations.RemoveCollection(MongoDatabase, Collection.CollectionNamespace.CollectionName);
                         }
                     }
                     break;
