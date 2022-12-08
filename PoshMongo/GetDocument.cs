1867eb7ec3ad097530502a5347d47ab704a33e08
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-08 09:03:34 -0600

    Updated AddDocumentCmdlet with new parameters and a little better logic that I will carry forward to the rest of the cmdlets

--- PoshMongo/GetDocument.cs
+++ PoshMongo/GetDocument.cs
@@ -11,73 +11,51 @@ namespace PoshMongo.Document
     [CmdletBinding(PositionalBinding = true, DefaultParameterSetName = "Default")]
     public class GetDocumentCmdlet : PSCmdlet
     {
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "DocumentId")]
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionId")]
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionNameId")]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionNameId")]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionId")]
         public string DocumentId { get; set; } = string.Empty;
 
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Filter")]
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionFilter")]
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionNameFilter")]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionFilter")]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionNameFilter")]
         public Hashtable Filter { get; set; } = new Hashtable();
 
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionNameFilter")]
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionNameId")]
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionNameId")]
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionNameFilter")]
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionNameList")]
         public string CollectionName { get; set; } = string.Empty;
+        [Parameter(Mandatory = true, Position = 2, ParameterSetName = "CollectionNameId")]
+        [Parameter(Mandatory = true, Position = 2, ParameterSetName = "CollectionNameFilter")]
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionNameList")]
+        public string DatabaseName { get; set; } = string.Empty;
 
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionId", ValueFromPipeline = true)]
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionFilter", ValueFromPipeline = true)]
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionId", ValueFromPipeline = true)]
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionFilter", ValueFromPipeline = true)]
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionList", ValueFromPipeline = true)]
         public IMongoCollection<BsonDocument>? MongoCollection { get; set; }
 
-        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "DocumentId")]
-        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Filter")]
-        [Parameter(Mandatory = false, Position = 3, ParameterSetName = "CollectionId")]
-        [Parameter(Mandatory = false, Position = 3, ParameterSetName = "CollectionFilter")]
+        [Parameter(Mandatory = false, Position = 2, ParameterSetName = "CollectionId")]
+        [Parameter(Mandatory = false, Position = 2, ParameterSetName = "CollectionFilter")]
         [Parameter(Mandatory = false, Position = 3, ParameterSetName = "CollectionNameId")]
         [Parameter(Mandatory = false, Position = 3, ParameterSetName = "CollectionNameFilter")]
-        [Parameter(Mandatory = false, Position = 2, ParameterSetName = "CollectionList")]
+        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "CollectionList")]
         [Parameter(Mandatory = false, Position = 2, ParameterSetName = "CollectionNameList")]
-        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "Default")] 
         public SwitchParameter HideId { get; set; }
         
-        [Parameter(Mandatory = false, Position = 0, ParameterSetName = "Default")]
-        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "CollectionList")]
-        [Parameter(Mandatory = false, Position = 1, ParameterSetName = "CollectionNameList")]
+        [Parameter(Mandatory = false, Position = 2, ParameterSetName = "CollectionList")]
+        [Parameter(Mandatory = false, Position = 3, ParameterSetName = "CollectionNameList")]
         public SwitchParameter List { get; set; }
+        private IMongoDatabase? MongoDatabase { get; set; }
+        private MongoClient? Client { get; set; } = null;
         protected override void ProcessRecord()
         {
-            if (MongoCollection == null)
-            {
-                if (string.IsNullOrEmpty(CollectionName))
-                {
-                    WriteVerbose("Get Collection");
-                    MongoCollection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
-                }
-                else
-                {
-                    WriteVerbose("Set Collection");
-                    MongoDatabaseBase MongoDatabase = (MongoDatabaseBase)SessionState.PSVariable.Get("Database").Value;
-                    MongoCollection = MongoDatabase.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings());
-                }
-            }
+            Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
             ObjectId objectId;
             switch (ParameterSetName)
             {
-                case "Filter":
-                    WriteObject(Operations.GetDocument(MongoCollection, Filter, HideId)); break;
-                case "DocumentId":
-                    if (ObjectId.TryParse(DocumentId, out objectId))
-                    {
-                        WriteObject(Operations.GetDocument(MongoCollection, objectId, HideId));
-                    }
-                    else
-                    {
-                        WriteObject(Operations.GetDocument(MongoCollection, DocumentId, HideId));
-                    }
-                    break;
                 case "CollectionNameId":
+                    //Get-MongoDBDocument [-DocumentId] <string> [-CollectionName] <string> [-DatabaseName] <string> [[-HideId]] [<CommonParameters>]
+                    MongoDatabase = Operations.GetDatabase(Client, DatabaseName);
+                    MongoCollection = Operations.GetCollection(MongoDatabase, CollectionName);
                     if (ObjectId.TryParse(DocumentId, out objectId))
                     {
                         WriteObject(Operations.GetDocument(MongoCollection, objectId, HideId));
@@ -88,23 +66,46 @@ namespace PoshMongo.Document
                     }
                     break;
                 case "CollectionNameFilter":
+                    //Get-MongoDBDocument [-Filter] <hashtable> [-CollectionName] <string> [-DatabaseName] <string> [[-HideId]] [<CommonParameters>]
+                    MongoDatabase = Operations.GetDatabase(Client, DatabaseName);
+                    MongoCollection = Operations.GetCollection(MongoDatabase, CollectionName);
                     WriteObject(Operations.GetDocument(MongoCollection, Filter, HideId));
                     break;
                 case "CollectionId":
-                    if (ObjectId.TryParse(DocumentId, out objectId))
+                    //Get-MongoDBDocument [-DocumentId] <string> [-MongoCollection] <IMongoCollection[BsonDocument]> [[-HideId]] [<CommonParameters>]
+                    if (MongoCollection != null)
                     {
-                        WriteObject(Operations.GetDocument(MongoCollection, objectId, HideId));
-                    }
-                    else
-                    {
-                        WriteObject(Operations.GetDocument(MongoCollection, DocumentId, HideId));
+                        if (ObjectId.TryParse(DocumentId, out objectId))
+                        {
+                            WriteObject(Operations.GetDocument(MongoCollection, objectId, HideId));
+                        }
+                        else
+                        {
+                            WriteObject(Operations.GetDocument(MongoCollection, DocumentId, HideId));
+                        }
                     }
                     break;
                 case "CollectionFilter":
-                    WriteObject(Operations.GetDocument(MongoCollection, Filter, HideId));
+                    //Get-MongoDBDocument [-Filter] <hashtable> [-MongoCollection] <IMongoCollection[BsonDocument]> [[-HideId]] [<CommonParameters>]
+                    if (MongoCollection != null)
+                    {
+                        WriteObject(Operations.GetDocument(MongoCollection, Filter, HideId));
+                    }
+                    break;
+                case "CollectionList":
+                    //Get-MongoDBDocument [-MongoCollection] <IMongoCollection[BsonDocument]> [[-HideId]] [[-List]] [<CommonParameters>]
+                    if (MongoCollection != null)
+                    {
+                        WriteObject(Operations.GetDocument(MongoCollection, HideId));
+                    }
+                    break;
+                case "CollectionNameList":
+                    //Get-MongoDBDocument [-CollectionName] <string> [-DatabaseName] <string> [[-HideId]] [[-List]] [<CommonParameters>]
+                    MongoDatabase = Operations.GetDatabase(Client, DatabaseName);
+                    MongoCollection = Operations.GetCollection(MongoDatabase, CollectionName);
+                    WriteObject(Operations.GetDocument(MongoCollection, HideId));
                     break;
                 default:
-                    WriteObject(Operations.GetDocument(MongoCollection, HideId));
                     break;
             }
         }
