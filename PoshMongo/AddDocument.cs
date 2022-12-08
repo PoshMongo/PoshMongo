ee8fcbd96ad03d580ea928c1b9bc28e0b2befc6a
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-08 10:04:09 -0600

    Updated AddDocumentCmdlet with new Parameters

--- PoshMongo/AddDocument.cs
+++ PoshMongo/AddDocument.cs
@@ -10,37 +10,33 @@ namespace PoshMongo.Document
     [CmdletBinding(PositionalBinding = true, DefaultParameterSetName = "Default")]
     public class AddDocumentCmdlet : PSCmdlet
     {
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Default")]
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionName")]
-        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Collection")]
-        public string Document { get; set; } = string.Empty;
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "CollectionName")]
+        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Collection")]
+        public string Document { get; set; } = string.Empty;
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "CollectionName")]
         public string CollectionName { get; set; } = string.Empty;
-        [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Collection", ValueFromPipeline = true)]
+        [Parameter(Mandatory = true, Position = 2, ParameterSetName = "CollectionName")] 
+        public string DatabaseName { get; set; } = string.Empty;
+        [Parameter(Mandatory = true, Position = 1, ParameterSetName = "Collection", ValueFromPipeline = true)]
         public IMongoCollection<BsonDocument>? MongoCollection { get; set; }
+        private IMongoDatabase? MongoDatabase { get; set; }
+        private MongoClient? Client { get; set; }
         protected override void ProcessRecord()
         {
-            if (MongoCollection == null)
-            {
-                if (!(string.IsNullOrEmpty(CollectionName)))
-                {
-                    MongoDatabaseBase MongoDatabase = (MongoDatabaseBase)SessionState.PSVariable.Get("Database").Value;
-                    MongoCollection = MongoDatabase.GetCollection<BsonDocument>(CollectionName, new MongoCollectionSettings());
-                }
-                else
-                {
-                    MongoCollection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
-                }
-            }
+            Client = (MongoClient)SessionState.PSVariable.Get("Client").Value;
             WriteVerbose(ParameterSetName);
             switch (ParameterSetName)
             {
                 case "CollectionName":
+                    MongoDatabase = Operations.GetDatabase(Client, DatabaseName);
+                    MongoCollection = Operations.GetCollection(MongoDatabase, CollectionName);
                     WriteObject(Operations.AddDocument(MongoCollection, Document));
                     break;
-                default:
-                    MongoCollection = (IMongoCollection<BsonDocument>)SessionState.PSVariable.Get("Collection").Value;
-                    WriteObject(Operations.AddDocument(MongoCollection, Document));
+                case "Collection":
+                    if (MongoCollection != null)
+                    {
+                        WriteObject(Operations.AddDocument(MongoCollection, Document));
+                    }
                     break;
             }
         }
