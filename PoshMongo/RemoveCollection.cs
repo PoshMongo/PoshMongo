8b7efbf24d012f4b3cbcde20729a1334c289abf8
Jeffrey Patton <jeffrey@patton-tech.com>
2022-12-09 20:36:49 -0600

    Set defaults and moved database to the top

--- PoshMongo/RemoveCollection.cs
+++ PoshMongo/RemoveCollection.cs
@@ -15,40 +15,32 @@ namespace PoshMongo.Collection
         [Parameter(Mandatory = true, Position = 1, ParameterSetName = "DatabaseName")]
         public string DatabaseName { get; set; } = string.Empty;
         [Parameter(Mandatory = true, Position = 0, ParameterSetName = "Collection", ValueFromPipeline = true)]
-        public IMongoCollection<BsonDocument>? Collection { get; set; }
-        private IMongoDatabase? MongoDatabase { get; set; }
+        public IMongoCollection<BsonDocument>? Collection { get; set; } = null;
+        private IMongoDatabase? MongoDatabase { get; set; } = null;
         private IMongoClient? Client { get; set; } = null;
         protected override void ProcessRecord()
         {
             Client = (IMongoClient)SessionState.PSVariable.Get("Client").Value;
-            WriteVerbose("ParameterSetName : " + ParameterSetName);
+            if (!(string.IsNullOrEmpty(CollectionName)))
+            {
+                MongoDatabase = Operations.GetDatabase(Client, DatabaseName);
+            }
+            if (Collection != null)
+            {
+                MongoDatabase = Operations.GetDatabase(Client, Collection.CollectionNamespace.DatabaseNamespace.DatabaseName);
+            }
             switch (ParameterSetName)
             {
                 case "DatabaseName":
-                    if (!(string.IsNullOrEmpty(CollectionName)) && !(string.IsNullOrEmpty(DatabaseName)))
+                    if (MongoDatabase != null)
                     {
-                        if (Client != null)
-                        {
-                            WriteVerbose("DatabaseName: " + DatabaseName);
-                            MongoDatabase = Operations.GetDatabase(Client, DatabaseName);
-                        }
-                        if (MongoDatabase != null)
-                        {
-                            WriteVerbose("CollectionName: " + CollectionName);
-                            Operations.RemoveCollection(MongoDatabase, CollectionName);
-                        }
+                        Operations.RemoveCollection(MongoDatabase, CollectionName);
                     }
                     break;
-                default:
-                    if (Collection != null)
+                case "Collection":
+                    if (MongoDatabase != null)
                     {
-                        WriteVerbose("DatabaseName: " + Collection.CollectionNamespace.DatabaseNamespace.DatabaseName);
-                        WriteVerbose("CollectionName: " + Collection.CollectionNamespace.CollectionName);
-                        MongoDatabase = Operations.GetDatabase(Client, Collection.CollectionNamespace.DatabaseNamespace.DatabaseName);
-                        if (MongoDatabase != null)
-                        {
-                            Operations.RemoveCollection(MongoDatabase, Collection.CollectionNamespace.CollectionName);
-                        }
+                        Operations.RemoveCollection(MongoDatabase, Collection.CollectionNamespace.CollectionName);
                     }
                     break;
             }
